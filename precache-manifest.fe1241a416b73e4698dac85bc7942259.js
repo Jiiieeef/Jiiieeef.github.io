7b9f47265e7b5ee2a6f790f81879f5d66d8e5af4
Jean-Franck Ballet <ballet.jean.franck@gmail.com>
2020-03-15 19:33:56 +0100

    Add udemy react first exo

---
+++ precache-manifest.fe1241a416b73e4698dac85bc7942259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc7a227200126bab6d40888103b0a235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f87721363f54a0b67f",
+    "url": "/static/css/main.43d2d998.chunk.css"
+  },
+  {
+    "revision": "e6a006491aafe5069e2a",
+    "url": "/static/js/2.1774a5c1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.1774a5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f87721363f54a0b67f",
+    "url": "/static/js/main.0abc509f.chunk.js"
+  },
+  {
+    "revision": "d7ad9ad8a004a4f86009",
+    "url": "/static/js/runtime-main.a799fa12.js"
+  }
+]);
